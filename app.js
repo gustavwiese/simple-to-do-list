ea62922e57a899b9779ca3d5c140ff70fd1acba6
gustavwiese <gusse12345@gmail.com>
2023-03-20 11:44:58 +0100

    tried some stuff

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+const newToDo = document.querySelector("#input-new-to-do");
+const list = document.querySelector("#to-do-list");
+
+function start() {
+  document.querySelector("#btn-add-item").addEventListener("click", addToDo);
+}
+
+function addToDo() {
+  const listElement = /*html*/ `
+<li>
+    <span>${newToDo.value}</span>
+    <button class="btn-delete">Delete</button>
+    <button class="btn-edit">Edit</button>
+</li>
+`;
+  list.insertAdjacentHTML("beforeend", listElement);
+  list.querySelector("li:last-child .btn-delete").addEventListener("click", removeToDo);
+  list.querySelector("li:last-child .btn-edit").addEventListener("click", editToDo);
+  //reset input
+  newToDo.value = "";
+  newToDo.focus();
+}
+
+function removeToDo() {
+  this.parentNode.remove();
+}
+
+function editToDo() {
+  console.log(this.parentNode);
+}
